d750fd12762a09996fa9d7dafe2a7cf52e6428c3
Nina Patafi <ninapatoofi@gmail.com>
2024-08-14 08:27:58 -0600

    Add to DressUpWidget and Filters + Add JSON data

--- src/components/DressUpWidget/DressUpWidget.jsx
+++ src/components/DressUpWidget/DressUpWidget.jsx
@@ -21,8 +21,39 @@ function DressUpWidget() {
           src="https://placehold.co/200x400"
         ></img>
       </div>
-      <div className="filter-category"></div>
-      <div className="filter-subcategory"></div>
+      <div className="filter-category">
+        <h5>category name placeholder</h5>
+        <button>button1</button>
+        <button>button2</button>
+        <button>button3</button>
+      </div>
+      <div className="filter-subcategory">
+        <div>
+          <h5>category</h5>
+          <h6>dresses</h6>
+          <h6>tops</h6>
+          <h6>bottoms</h6>
+          <h6>outerwear</h6>
+          <h6>shoes</h6>
+          <h6>bags & purses</h6>
+        </div>
+        <div>
+          <h5>color</h5>
+        </div>
+        <div className="size-selection">
+          <h5>size</h5>
+          <button>button1</button>
+          <button>button2</button>
+          <button>button3</button>
+        </div>
+        <div>
+          <h5>price</h5>
+          <h6>under $15</h6>
+          <h6>$15-30</h6>
+          <h6>$30-60</h6>
+          <h6>$60+</h6>
+        </div>
+      </div>
     </div>
   );
 }
--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,11 +1,7 @@
 import "./Footer.scss";
 
 function Footer() {
-  return (
-    <footer className="footer">
-      <h2> footer placeholder</h2>
-    </footer>
-  );
+  return <footer className="footer"></footer>;
 }
 
 export default Footer;
